7bd98debc47acb1d041077b288f3ffe836bf1f5e
francisco28 <fgallegosgo@gmail.com>
2020-06-05 12:49:49 -0600

    consult to API and reading the results

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 import Header from './components/Header';
 import Form from './components/Form';
 
@@ -7,6 +7,23 @@ function App() {
 
   //define the category and news
   const [ category, saveCategory ] = useState('');
+  
+  //state to save the news
+  const [ news, saveNews ] = useState([]);
+
+
+  //using useEffect para cuando detecte un cambio en "categoria" se vuelva a ejecutar el componente
+  useEffect(() => {
+      const consultAPI = async () => {
+          const url = `https://newsapi.org/v2/top-headlines?country=mx&category=${category}&apiKey=45ef01eae29e45318b02524541456fd9`;
+
+          const answer = await fetch(url);
+          const news = await answer.json();
+
+          saveNews(news.articles);
+      }
+      consultAPI();
+  }, [category]);
 
   return (
       <Fragment>
